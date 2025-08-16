75b20fd7adea2209a24774985a26cb04a3b796db
sudipmaji366 <sudipmaji366@gmail.com>
2025-08-16 21:47:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "sudipmaji_vpc" {
+resource "aws_vpc" "sudip_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "sudipmaji-vpc"
+    Name = "sudip-vpc"
   }
 }
 
-resource "aws_subnet" "sudipmaji_subnet" {
+resource "aws_subnet" "sudip_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.sudipmaji_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.sudipmaji_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.sudip_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.sudip_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "sudipmaji-subnet-${count.index}"
+    Name = "sudip-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "sudipmaji_igw" {
-  vpc_id = aws_vpc.sudipmaji_vpc.id
+resource "aws_internet_gateway" "sudip_igw" {
+  vpc_id = aws_vpc.sudip_vpc.id
 
   tags = {
-    Name = "sudipmaji-igw"
+    Name = "sudip-igw"
   }
 }
 
-resource "aws_route_table" "sudipmaji_route_table" {
-  vpc_id = aws_vpc.sudipmaji_vpc.id
+resource "aws_route_table" "sudip_route_table" {
+  vpc_id = aws_vpc.sudip_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.sudipmaji_igw.id
+    gateway_id = aws_internet_gateway.sudip_igw.id
   }
 
   tags = {
-    Name = "sudipmaji-route-table"
+    Name = "sudip-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.sudipmaji_subnet[count.index].id
-  route_table_id = aws_route_table.sudipmaji_route_table.id
+  subnet_id      = aws_subnet.sudip_subnet[count.index].id
+  route_table_id = aws_route_table.sudip_route_table.id
 }
 
-resource "aws_security_group" "sudipmaji_cluster_sg" {
-  vpc_id = aws_vpc.sudipmaji_vpc.id
+resource "aws_security_group" "sudip_cluster_sg" {
+  vpc_id = aws_vpc.sudip_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "sudipmaji_cluster_sg" {
   }
 
   tags = {
-    Name = "sudipmaji-cluster-sg"
+    Name = "sudip-cluster-sg"
   }
 }
 
-resource "aws_security_group" "sudipmaji_node_sg" {
-  vpc_id = aws_vpc.sudipmaji_vpc.id
+resource "aws_security_group" "sudip_node_sg" {
+  vpc_id = aws_vpc.sudip_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,42 +82,42 @@ resource "aws_security_group" "sudipmaji_node_sg" {
   }
 
   tags = {
-    Name = "sudipmaji-node-sg"
+    Name = "sudip-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "sudipmaji" {
-  name     = "sudipmaji-cluster"
-  role_arn = aws_iam_role.sudipmaji_cluster_role.arn
+resource "aws_eks_cluster" "sudip" {
+  name     = "sudip-cluster"
+  role_arn = aws_iam_role.sudip_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.sudipmaji_subnet[*].id
-    security_group_ids = [aws_security_group.sudipmaji_cluster_sg.id]
+    subnet_ids         = aws_subnet.sudip_subnet[*].id
+    security_group_ids = [aws_security_group.sudip_cluster_sg.id]
   }
 }
 
-resource "aws_eks_node_group" "sudipmaji" {
-  cluster_name    = aws_eks_cluster.sudipmaji.name
-  node_group_name = "sudipmaji-node-group"
-  node_role_arn   = aws_iam_role.sudipmaji_node_group_role.arn
-  subnet_ids      = aws_subnet.sudipmaji_subnet[*].id
+resource "aws_eks_node_group" "sudip" {
+  cluster_name    = aws_eks_cluster.sudip.name
+  node_group_name = "sudip-node-group"
+  node_role_arn   = aws_iam_role.sudip_node_group_role.arn
+  subnet_ids      = aws_subnet.sudip_subnet[*].id
 
   scaling_config {
-    desired_size = 1
+    desired_size = 3
     max_size     = 3
-    min_size     = 1
+    min_size     = 3
   }
 
-  instance_types = ["t2.micro"]
+  instance_types = ["t2.medium"]
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.sudipmaji_node_sg.id]
+    source_security_group_ids = [aws_security_group.sudip_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "sudipmaji_cluster_role" {
-  name = "sudipmaji-cluster-role"
+resource "aws_iam_role" "sudip_cluster_role" {
+  name = "sudip-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,13 +135,13 @@ resource "aws_iam_role" "sudipmaji_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "sudipmaji_cluster_role_policy" {
-  role       = aws_iam_role.sudipmaji_cluster_role.name
+resource "aws_iam_role_policy_attachment" "sudip_cluster_role_policy" {
+  role       = aws_iam_role.sudip_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "sudipmaji_node_group_role" {
-  name = "sudipmaji-node-group-role"
+resource "aws_iam_role" "sudip_node_group_role" {
+  name = "sudip-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -159,17 +159,18 @@ resource "aws_iam_role" "sudipmaji_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "sudipmaji_node_group_role_policy" {
-  role       = aws_iam_role.sudipmaji_node_group_role.name
+resource "aws_iam_role_policy_attachment" "sudip_node_group_role_policy" {
+  role       = aws_iam_role.sudip_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "sudipmaji_node_group_cni_policy" {
-  role       = aws_iam_role.sudipmaji_node_group_role.name
+resource "aws_iam_role_policy_attachment" "sudip_node_group_cni_policy" {
+  role       = aws_iam_role.sudip_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "sudipmaji_node_group_registry_policy" {
-  role       = aws_iam_role.sudipmaji_node_group_role.name
+resource "aws_iam_role_policy_attachment" "sudip_node_group_registry_policy" {
+  role       = aws_iam_role.sudip_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
